c5a48c74708eacf5c58b066ae1ca842daa007af0
CHONDO DIO <you@example.com>
2024-03-05 11:41:03 +0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem"
+import InputArea from "./InputArea";
+
+function App() {
+ 
+  const [items, setItems] = useState([]);
+
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      < InputArea
+      onAdd = {addItem} />
+      <div>
+        <ul>
+          {items.map(todoItem => (
+            < ToDoItem text={todoItem}/>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+
+}
+
+
+export default App;
